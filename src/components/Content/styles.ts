ccb147e219e32b9b33dceb7c119461d57aeff080
elton de campos <eltoncampos36@gmail.com>
2021-05-28 00:07:48 -0300

    style: add some styles to content component

---
+++ src/components/Content/styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+
+export const Container = styled.section`
+    grid-area: content;
+    background-color:#EEF2F4;
+`;
+
+export const TopContent = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+
+    padding: 15px 20px;
+    > div {
+        display: flex;
+    }
+`
+
+export const Cell = styled.div`
+
+`;
