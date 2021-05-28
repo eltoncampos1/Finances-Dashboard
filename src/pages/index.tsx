a03e44f2ba482c3c8bdd9489a5c645b8af16fb5c
elton de campos <eltoncampos36@gmail.com>
2021-05-28 00:07:31 -0300

    chore: pass cell props to static props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,45 @@
 import { Dashboard } from "../layouts/DashBoard";
+import { GetStaticProps } from "next";
+import api from "../utils/api";
 
-export default function Home() {
+interface DataApi {
+  data: {
+    snapshotByPortfolio: {
+      equity: number;
+      valueApplied: number;
+      equityProfit: number;
+      percentageProfit: number;
+      indexerValue: number;
+      percentageOverIndexer: number;
+    };
+  };
+}
+
+export default function Home({ data }: DataApi) {
   return (
     <div>
-      <Dashboard />
+      <Dashboard
+        equity={data.snapshotByPortfolio.equity}
+        valueApplied={data.snapshotByPortfolio.valueApplied}
+        equityProfit={data.snapshotByPortfolio.equityProfit}
+        percentageProfit={data.snapshotByPortfolio.percentageProfit}
+        indexerValue={data.snapshotByPortfolio.indexerValue}
+        percentageOverIndexer={data.snapshotByPortfolio.percentageOverIndexer}
+      />
     </div>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const data = await api.get("/").then((response) => {
+    return response.data.data;
+  });
+
+  console.log(data);
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
