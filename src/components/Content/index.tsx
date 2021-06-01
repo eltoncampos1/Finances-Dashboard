0cb71897f657ef04837aabec32221498972e32f6
elton de campos <eltoncampos36@gmail.com>
2021-06-01 01:14:45 -0300

    chore: create tabble cell

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,6 +1,7 @@
-// import Image from "next/image";
+import Image from "next/image";
 
 import { Cells } from "../Cells";
+import { Row } from "../Row";
 import * as S from "./styles";
 
 interface DataApi {
@@ -33,25 +34,36 @@ export function Content({
           percentageOverIndexer={percentageOverIndexer}
         />
       </S.TopContent>
+
+      <S.TableCell>
+        <S.Header>
+          <h2>Minhas rendas fixas</h2>
+          <div>
+            <select name="ordenar" id="ordenar">
+              <option value="">Ordenar por</option>
+              <option value="">Ordenar por</option>
+              <option value="">Ordenar por</option>
+              <option value="">Ordenar por</option>
+              <option value="">Ordenar por</option>
+              <option value="">Ordenar por</option>
+              <option value="">Ordenar por</option>
+            </select>
+            <div className="input">
+              <Image
+                objectFit="contain"
+                className="lupa"
+                src="/assets/lupa.png"
+                alt="Search"
+                width={15}
+                height={15}
+              />
+              <input type="text" />
+            </div>
+          </div>
+        </S.Header>
+
+        <Row />
+      </S.TableCell>
     </S.Container>
   );
 }
-
-//  <h2>Minhas rendas fixas</h2>
-
-//     <div>
-//       <select name="ordenar" id="ordenar">
-//         <option value="">Ordenar por</option>
-//         <option value="">Ordenar por</option>
-//         <option value="">Ordenar por</option>
-//         <option value="">Ordenar por</option>
-//         <option value="">Ordenar por</option>
-//         <option value="">Ordenar por</option>
-//         <option value="">Ordenar por</option>
-//       </select>
-
-//       <div className="input">
-//         <Image src="/assets/lupa.png" alt="Search" width={15} height={15} />
-//         <input type="text" />
-//       </div>
-//     </div>
